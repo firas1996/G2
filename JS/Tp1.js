83ac656c44dc578fad742a87dba0290c3d628b5e
firas hamila <firas.hm.1996@gmail.com>
2023-11-13 11:48:08 +0100

    ww

--- JS/Tp1.js
+++ JS/Tp1.js
@@ -58,26 +58,68 @@ const arrayReverse = (t) => {
 console.log(strReverse("abcdef"));
 console.log(arrayReverse([1, 2, 3, 4]));*/
 
-const subarray = (t1, t2) => {
-  let t3 = [];
-  let k = 0;
-  for (let i = 0; i <= t2.length - 1; i++) {
-    let v = false;
-    for (let j = 0; j <= t1.length - 1; j++) {
-      if (t1[j] == t2[i]) {
-        v = true;
-      }
-    }
-    if (!v) {
-      t3[k] = t2[i];
-      k++;
-    }
+// const subarray = (t1, t2) => {
+//   let t3 = [];
+//   let k = 0;
+//   for (let i = 0; i <= t2.length - 1; i++) {
+//     let v = false;
+//     for (let j = 0; j <= t1.length - 1; j++) {
+//       if (t1[j] == t2[i]) {
+//         v = true;
+//       }
+//     }
+//     if (!v) {
+//       t3[k] = t2[i];
+//       k++;
+//     }
+//   }
+//   return t3;
+// };
+// const subarray2 = (t1, t2) => {
+//   return t2.filter((item) => !t1.includes(item));
+// };
+// let t2 = [1, 5, 4, 2, 8, 9, 3, 11, 2, 5];
+// let t1 = [1, 2, 3];
+// console.log(subarray2(t1, t2));
+//const getMax = (a, b, c) => {
+//   let max = a;
+//   if (b > max) {
+//     max = b;
+//   }
+//   if (c > max) {
+//     max = c;
+//   }
+//   return max;
+// };
+//return Math.max(a, b, c);
+//};
+
+//console.log(getMax(1, 5, 2));
+//let t = [1, 2, 9, 4, 5];
+//const maxarray = (t) => {
+//   max = t[0];
+//   for (i = 1; i < t.length; i++) {
+//     if (t[i] > max) {
+//       max = t[i];
+//     }
+//   }
+//   return max;
+//return Math.max(...t);
+//};
+//console.log(maxarray(t));
+
+/////////////////
+const check = (m) => {
+  if (m % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
   }
-  return t3;
 };
-const subarray2 = (t1, t2) => {
-  return t2.filter((item) => !t1.includes(item));
+console.log(check(2));
+console.log(check(7));
+const represenet = (obj) => {
+  return `${obj.name} is ${obj.age} . the qddress is ${obj.address}`;
 };
-let t2 = [1, 5, 4, 2, 8, 9, 3, 11, 2, 5];
-let t1 = [1, 2, 3];
-console.log(subarray2(t1, t2));
+const prs = { name: "wassim", age: "27", address: "rue " };
+console.log(represenet(prs));
